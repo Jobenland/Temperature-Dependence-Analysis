c3ce632867ef8c2d67338bdbc3a8c497b2619d4a
Jonathan Obenland <jobenland1@gmail.com>
2019-06-26 10:56:27 -0400

    Code Cleanup

--- extraval.py
+++ extraval.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
 '''
-VERSION 2.0 - OPTIMIZED
-
+T E M P E R A T U R E   D E P E N D E N C E   A N A L Y S I S
  This Program is meant to add 7 coulumns with various data from a given set of files
+ and add them to a CSV
 '''
 
 __author__ = "Jonathan Obenland"
 __copyright__ = "Copyright 2019, MEII"
-__credits__ = ["Jonathan Obenland", "Ian Robinson", "Mikethewatchguy"]
+__credits__ = ["Jonathan Obenland","Mikethewatchguy"]
 __license__ = "GPL"
 __version__ = "1.0.0"
 __maintainer__ = "Jonathan Obenland"
@@ -16,6 +16,7 @@ __status__ = "Production"
 
 
 #all imports
+#TODO clean this up it looks terrible
 import sys
 import tkinter
 from tkinter import ttk
@@ -38,19 +39,10 @@ from bokeh.models import ColumnDataSource
 import fnmatch
 import zipfile
 import shutil
-
-
-#checks to ensure that version is up to date
 if sys.version_info[0] >= 3:
     import PySimpleGUI as sg
 else:
     import PySimpleGUI27 as sg
-
-#Creation of the array
-#each experiment has its own array
-#dynamic = galvanodynamics
-#static = galvanostatic
-#impedance = impedance AC (not DC)
 
 arrayOfImpFiles = []
 impedanceElectrodeASRList = []
@@ -59,6 +51,8 @@ impedanceOhmicList = []
 impedanceTimeInSecounds = []
 impedanceTimeInHours = []
 listOfCSV = []
+
+#Main Winodw that displays starting data
 def mainWin():
     form_rows = [[sg.Text("Enter the file name, file path to save, and the location of the files")],
                  [sg.Text('Enter the the area', size=(21,1)),sg.InputText(key = 'area')],
@@ -67,9 +61,8 @@ def mainWin():
                  [sg.Button('start'),sg.Exit()]]
 
     window = sg.Window("added columns program")
-
     event,values = window.Layout(form_rows).Read()
-
+    #start the event listener
     while True:
 
         if event is None or event == 'Exit':
@@ -83,6 +76,8 @@ def mainWin():
                 window.Close()
                 sg.Popup("ERROR: Insufficent data or Null Pointers given")
                 mainWin()
+            
+            #if both areas are populated then
             if area != '' or directory != '':
                 window.Close()
                 zDir = unzipper(directory,zipp)
@@ -91,8 +86,14 @@ def mainWin():
                 sg.Popup("complete")
                 mainWin()
                 #impedanceReader(area,directory)
+
+#Linear definition to create folder
+#navigate to folder, extract all the data into another folder
+#move all .z files to a new folder
+#create CSV's there
 def unzipper(directory,zipp):
 
+    #create new folder to put extracted data in
     os.chdir(directory)
     newFolder = 'Extracted'
     try:
@@ -101,18 +102,21 @@ def unzipper(directory,zipp):
     except OSError:
         print('Error')
 
-
+    #extract all data to the new folder
     newDirName = directory + '/' + newFolder
     os.chdir(newDirName)
     with ZipFile(zipp,'r') as zipObj:
         zipObj.extractall(newDirName)
     
+    #walk the OS path of all the folders and find all .zip files
+    #extract everything it finds
     pattern = '*.zip'
     for root, dirs, files in os.walk(newDirName):
         for filename in fnmatch.filter(files, pattern):
             print(os.path.join(root,filename))
             zipfile.ZipFile(os.path.join(root,filename)).extractall(os.path.join(root, os.path.splitext(filename)[0]))
     
+    #pulling all .z files from the extracted folders into a new folder
     combAll = 'Extracted Z Files'
     try:
         if not os.path.exists(combAll):
@@ -131,98 +135,14 @@ def unzipper(directory,zipp):
         print("Same File Name Found")
     return(newZDir)
 
-def impedanceReader(area, directory):
-
-    stringZPrime,stringZDoublePrime,dateTimeList,floatTime = ([]for i in range(4))
-    stringFreq=[]
-    stringTS = []
-    impedanceFileCount=0
-    global impedanceElectrodeASRList
-    global impedanceTotalASRList
-    global impedanceOhmicList
-    global impedanceTimeInSecounds
-    global impedanceTimeInHours
-    global arrayOfImpFiles
-
-    impFiles = listOfImpFiles(area,directory)
-    os.chdir(directory)
-    for file in impFiles:
-
-        with open(file, 'r') as f:
-
-            for row in f:
-                if 'End Header:' in row:
-                    for x in f:
-                        stringFreq.append(x.split('\t')[0])
-                        stringTS.append(x.split('\t')[3])
-                        stringZPrime.append(x.split('\t')[4])
-                        stringZDoublePrime.append(x.split('\t')[5])
-                    intFreq = [float(i) for i in stringFreq]
-                    intTS = [float(i) for i in stringTS]
-                    intZPrime = [float(i) for i in stringZPrime]
-                    intzDoublePrime = [float(i) for i in stringZDoublePrime]
-                    zDoublePrimeShort = intzDoublePrime[5:25]
-                    zDoublePrimeABS = [abs(i) for i in zDoublePrimeShort]
-            ohmicZDoublePrime = min(zDoublePrimeABS)
-            ohmicZPrime = min(z)
-            if ohmicZDoublePrime in intzDoublePrime:
-                ohmicZPrimeIndex = intzDoublePrime.index(ohmicZDoublePrime)
-                ohmicZPrime = intZPrime[ohmicZPrimeIndex]
-            else:
-                ohmicZPrime = intzDoublePrime.index(-ohmicZDoublePrime)
-
-            #ammending things and setting things   
-            ohmicMin = intZPrime[ohmicZPrimeIndex]
-            totalASR = max(intZPrime[ohmicZPrimeIndex:])
-            electrodeASR = totalASR-ohmicMin
-            impedanceElectrodeASRList.append(electrodeASR)
-            impedanceTotalASRList.append(totalASR) 
-            impedanceOhmicList.append(ohmicMin)
-            timeT = getTime(file)
-            dateD = getDate(file)
-            dateTimeString = dateD + ' ' + timeT
-            dateTimeFormat = datetime.datetime.strptime(dateTimeString, '%d/%m/%Y %I:%M:%S %p')
-            dateTimeList.append(dateTimeFormat)
-            correctedTime = time.mktime(dateTimeFormat.timetuple())
-            floatTime.append(float(correctedTime))
-            impedanceTimeInSecounds.append(correctedTime)
-            minTime = min(impedanceTimeInSecounds)
-            impedanceTimeInHours = [((impedanceTimeInSecounds[i]-minTime)/3600) for i in range(len(impedanceTimeInSecounds))]
-            intArea = (float(area))
-
-            
-            #ohmicARC = ((ohmicZPrime)*(intArea))
-
-            
-            impedanceZPrimeOC = [((intZPrime[i])-(ohmicZPrime)) for i in range(len(intZPrime))]
-            impedanceZPrimeARC= [((impedanceZPrimeOC[i])*(intArea)) for i in range(len(intZPrime))]
-            impedanceZDoublePrimeARC = [((intzDoublePrime[i])*(intArea)) for i in range(len(intzDoublePrime))]
-            #impedanceZPrimeOhmicOC = [((impedanceZPrimeOC[i])*(intArea)) for i in range(len(impedanceZPrimeOC))]
-
-
-            print("stop")
-
-    print(len(impedanceTimeInSecounds))
-
-    print(len(impedanceZDoublePrimeARC))
-    print(len(impedanceZPrimeARC))
-    print(len(impedanceZPrimeOC))
-    
-    print(len(impedanceOhmicList))
-    print(len(impedanceElectrodeASRList))
-
-    createCSV(directory,intFreq,impedanceElectrodeASRList,impedanceOhmicList,impedanceTotalASRList,impedanceZPrimeOC,impedanceZPrimeARC,impedanceZDoublePrimeARC)
-
-            #impedanceZPrimeOC=[((impedanceZPrimeARC[i])-(ohmicZPrime[i])) for i in range(len(impedanceZPrimeARC))]
-            #print (impedanceZPrimeOC)
-
-
+#Definiton to do that math on the .z files in the new folder
 def testing(area,directory):
     stringFreq = []
     stringTS = []
     stringZPrime = []
     stringZDoublePrime = []
 
+    #error checking to make sure no rouge non-Impedance Files got in
     impFiles = listOfImpFiles(area,directory)
     os.chdir(directory)
 
@@ -239,9 +159,11 @@ def testing(area,directory):
                     intTS = [float(i) for i in stringTS]
                     intZPrime = [float(i) for i in stringZPrime]
                     intZDoublePrime = [float(i) for i in stringZDoublePrime]
+                    #calls the function to calculate the range of the values
+                    #needed to find the ohmin
                     startRange,endRange = getRange(intZDoublePrime)
+                    #-1 is the "error code" used when data never goes below the x-axis
                     if endRange == -1:
-                        
                         zDoublePrimeShort = max(intZDoublePrime)
                         zDoublePrimeABS = zDoublePrimeShort
                         ohmicZDoublePrime = zDoublePrimeShort
@@ -252,42 +174,38 @@ def testing(area,directory):
                         zDoublePrimeABS= [abs(i) for i in zDoublePrimeShort]
                         ohmicZDoublePrime = min(zDoublePrimeABS)
             intArea = (float(area))
-            
-
             if ohmicZDoublePrime in intZDoublePrime:
                 ohmicZPrimeIndex = intZDoublePrime.index(ohmicZDoublePrime)
                 ohmicZPrime = intZPrime[ohmicZPrimeIndex]
             else:
                 ohmicZPrimeIndex = intZDoublePrime.index(-ohmicZDoublePrime)
                 ohmicZPrime = intZPrime[ohmicZPrimeIndex]
-            
             zPrimeOC = [((intZPrime[i])-(ohmicZPrime)) for i in range(len(intZPrime))]
             zPrimeARC= [((zPrimeOC[i])*(intArea)) for i in range(len(intZPrime))]
             zDoublePrimeARC = [((intZDoublePrime[i])*(intArea)) for i in range(len(intZDoublePrime))]
-
             positiveZDoublePrime = [-x for x in zDoublePrimeARC]
             createCSV(file,directory,intTS, intFreq, intZPrime,intZDoublePrime,zPrimeOC,zPrimeARC,zDoublePrimeARC,positiveZDoublePrime)
             print("rp?")
 
-            
-
-
-
+#definition to get the range of the data
+#will get the last negative point and the last positive point
+#if neither exist return "error codes"
 def getRange(intZDoublePrime):
     PG=[]
     NG=[]
     t=0
+    #first part tests point and the next point
+    #to find positive then negative
     for i in range(len(intZDoublePrime)):
         if intZDoublePrime[i] > 0:
             t+1
         elif intZDoublePrime[i] < 0:
             if intZDoublePrime[i-1] >0:
                 indexx = i-1
-                #for j in range(i):
-                    #del intZDoublePrime[j]
                 break
     
-    
+    #makes lists for all the positive corresponding index
+    #makes lists for all the negative corresponding index
     for i in range(len(intZDoublePrime)):
         if intZDoublePrime[i] < 0:
             PG.append(i)
@@ -296,7 +214,9 @@ def getRange(intZDoublePrime):
         elif intZDoublePrime[i] == 0:
             OH = intZDoublePrime[i]
             break
-    
+   
+   #checking to make sure that the graph has both
+   #positive and negative values
     if NG == [] and PG != []:
         startRange = min(intZDoublePrime)
         endRange = -1
@@ -306,13 +226,8 @@ def getRange(intZDoublePrime):
         endRange = startRange + 1
     return(startRange,endRange)
 
-
-
-                    
-
-
-
-    
+#creates the CSV's and appends them to a list for later
+# excel use   
 def createCSV (file,directory,intTS, intFreq, intZPrime,intZDoublePrime,zPrimeOC,zPrimeARC,zDoublePrimeARC,positiveZDoublePrime):
     dirpath = os.chdir(directory)
     fileN = file +'.csv'
@@ -321,13 +236,8 @@ def createCSV (file,directory,intTS, intFreq, intZPrime,intZDoublePrime,zPrimeOC
     df = pd.DataFrame(data=dataf)
     df.to_csv(fileN,index = False)
     listOfCSV.append(fileN)
-    #writer = pd.ExcelWriter('yourfile.xlsx', engine='xlsxwriter')
-    #df = pd.read_csv(fileN)
-    #df.to_excel(writer, sheet_name='Impedance')
-    #writer.save()
-    #listofCSV.append(fileN)
 
-
+#definition to make an array of Files that are the experiment "Impedance"
 def listOfImpFiles(area,directory):
     os.chdir(directory)
     fileList = os.listdir(directory)
@@ -348,6 +258,7 @@ def listOfImpFiles(area,directory):
                                     arrayOfImpFiles.append(file)
     return arrayOfImpFiles
 
+#gets the time in the file
 def getTime(file):
     with open(file,'r',encoding = "ISO-8859-1") as f:
         for row in f:
@@ -363,9 +274,11 @@ def getDate(file):
                 strippedDate= row.strip('Date: \n')
                 return strippedDate
 
+#definition to generate a EXCEL file
+# with multiple sheet names as all the .z files from
+# different temperatures
 def generateSheets(listOfCSV, fileName):
-    writer = pd.ExcelWriter(fileName+'.xlsx', engine = 'xlsxwriter')
-    
+    writer = pd.ExcelWriter(fileName+'.xlsx', engine = 'xlsxwriter') 
     for csv in listOfCSV:
         csvSplitList = csv.split('_')
         print(csvSplitList)
@@ -378,8 +291,7 @@ def generateSheets(listOfCSV, fileName):
         df = pd.read_csv(csv)
         df.to_excel(writer, sheet_name=stringName)
     writer.save() 
-                    
 
-            
+#magic method to call main           
 if __name__ == '__main__':
     mainWin()
